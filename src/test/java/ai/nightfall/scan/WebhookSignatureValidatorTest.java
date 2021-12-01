e75904aa81b3a12a530a9944a29f77170629a1da
Evan Fuller <evan.m.fuller@gmail.com>
2021-12-01 08:24:59 -0800

    Fix webhook validator (#27)
    The webhook validator should consume the request timestamp as epoch seconds, not as an RFC3339 date string.

--- src/test/java/ai/nightfall/scan/WebhookSignatureValidatorTest.java
+++ src/test/java/ai/nightfall/scan/WebhookSignatureValidatorTest.java
@@ -17,10 +17,10 @@ public class WebhookSignatureValidatorTest {
 
     @Test
     public void testHappyPath() {
-        String timestamp = "2021-10-04T17:30:43.42Z";
+        String timestamp = "1633368643";
         String reqBody = "hello world foo bar goodnight moon";
         byte[] secret = "super-secret-shhhh".getBytes(StandardCharsets.UTF_8);
-        String expectedSignature = "45bf0200a2cbc02ae2595fcf9ba4a2b262d836672c4a26fc3300cc54353587cd";
+        String expectedSignature = "641ada412da02d7df7ca59e94a556b7f5683374db614565ad3d99da1a9a779fb";
 
         WebhookSignatureValidator validator = new WebhookSignatureValidator(reallyLongTime);
         boolean result = validator.validate(reqBody, secret, expectedSignature, timestamp);
@@ -29,10 +29,22 @@ public class WebhookSignatureValidatorTest {
 
     @Test
     public void testRequestTooOld() {
-        String timestamp = "2021-10-04T17:30:43.42Z";
+        String timestamp = "1633368643";
         String reqBody = "hello world foo bar goodnight moon";
         byte[] secret = "super-secret-shhhh".getBytes(StandardCharsets.UTF_8);
-        String expectedSignature = "45bf0200a2cbc02ae2595fcf9ba4a2b262d836672c4a26fc3300cc54353587cd";
+        String expectedSignature = "641ada412da02d7df7ca59e94a556b7f5683374db614565ad3d99da1a9a779fb";
+
+        WebhookSignatureValidator validator = new WebhookSignatureValidator(Duration.ofMinutes(1));
+        boolean result = validator.validate(reqBody, secret, expectedSignature, timestamp);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testRequestTooFarInTheFuture() {
+        String timestamp = "4789042243";
+        String reqBody = "hello world foo bar goodnight moon";
+        byte[] secret = "super-secret-shhhh".getBytes(StandardCharsets.UTF_8);
+        String expectedSignature = "641ada412da02d7df7ca59e94a556b7f5683374db614565ad3d99da1a9a779fb";
 
         WebhookSignatureValidator validator = new WebhookSignatureValidator(Duration.ofMinutes(1));
         boolean result = validator.validate(reqBody, secret, expectedSignature, timestamp);
@@ -41,7 +53,7 @@ public class WebhookSignatureValidatorTest {
 
     @Test
     public void testSignatureMismatch() {
-        String timestamp = "2021-10-04T17:30:43.42Z";
+        String timestamp = "1633368643";
         String reqBody = "hello world foo bar goodnight moon";
         byte[] secret = "super-secret-shhhh".getBytes(StandardCharsets.UTF_8);
         String incorrectSignature = "e05aa9a373d652b6a38fdb0e093cca3eca3d6dd803a50dbe8b98b137fd20fe87";
